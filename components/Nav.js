0c1a8197bb80204bdf7c74118d1b2b4eb90a082e
MVAodhan <hamilton.aodhan@gmail.com>
2021-12-30 15:55:09 +1300

    added log out/login function and button

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,33 @@
 import Link from 'next/link';
 
-import { Box, ListItem, UnorderedList } from '@chakra-ui/react';
+import { Box, ListItem, UnorderedList, Button } from '@chakra-ui/react';
+
+import { sessionAtom } from '../atoms';
+import { useAtom } from 'jotai';
+
+import { useRouter } from 'next/router';
+
+import { useSupabase } from '../hooks/useSupabase.js';
 
 const Nav = ({ hProp }) => {
+  const [session, setSession] = useAtom(sessionAtom);
+
+  const supabase = useSupabase();
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    if (!session) {
+      router.push('/log-in');
+
+      return;
+    }
+
+    let { error } = await supabase.auth.signOut();
+
+    if (!error) {
+      router.reload();
+    }
+  };
   return (
     <>
       <Box
@@ -16,23 +41,32 @@ const Nav = ({ hProp }) => {
         pt="20px"
         pb="20px"
       >
-        <UnorderedList
-          styleType="none"
-          d="flex"
-          pr="50px"
-          fontFamily="Alfa Slab One"
-        >
-          <ListItem pr="50px">
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="/add-episode">
-              <a>Add Episode</a>
-            </Link>
-          </ListItem>
-        </UnorderedList>
+        <Box w="50%" h="100%" d="flex" justifyContent="flex-start" pl="50px">
+          <Button onClick={handleLogin} color="black">
+            {!session ? 'Log in' : 'Log out'}
+          </Button>
+        </Box>
+        <Box w="50%" d="flex" justifyContent="flex-end">
+          <UnorderedList
+            styleType="none"
+            d="flex"
+            pr="50px"
+            fontFamily="Alfa Slab One"
+          >
+            <ListItem pr="50px">
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </ListItem>
+            {session && (
+              <ListItem>
+                <Link href="/add-episode">
+                  <a>Add Episode</a>
+                </Link>
+              </ListItem>
+            )}
+          </UnorderedList>
+        </Box>
       </Box>
     </>
   );
